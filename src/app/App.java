5e99f7f05cb2d0cfc539912b00cb54f228933afa
LR-LR <elhaire.aboudou@gmail.com>
2019-11-14 14:11:03 +0100

    beginning of first TP

--- src/app/App.java
+++ src/app/App.java
@@ -2,6 +2,16 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        char reponse = 'O';
+
+        System.out.println("CONVERTISSEUR DÉGRES CELSIUS ET DEGRÉS FAHRENHEIT");
+        System.out.println("------------------------------------------------");
+
+        do {
+            System.out.println("Choisissez le mode de conversion :");
+            System.out.println("1 - Convertisseur °C - °F");
+            System.out.println("2 - Convertisseur °F - °C");
+            int mode;
+        } while (reponse == 'O');
     }
 }
