93f2b54d97d31e670237f9e267b7a8abf0f65070
LR-LR <elhaire.aboudou@outlook.com>
2019-11-15 11:59:34 +0100

    fin premier TP

--- src/app/App.java
+++ src/app/App.java
@@ -1,17 +1,52 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        char reponse = 'O';
+        Scanner sc = new Scanner(System.in);
+        char mode, reponse;
+        double temperature, conversion;
 
         System.out.println("CONVERTISSEUR DÉGRES CELSIUS ET DEGRÉS FAHRENHEIT");
         System.out.println("------------------------------------------------");
 
         do {
-            System.out.println("Choisissez le mode de conversion :");
-            System.out.println("1 - Convertisseur °C - °F");
-            System.out.println("2 - Convertisseur °F - °C");
-            int mode;
-        } while (reponse == 'O');
+            do {
+                System.out.println("Choisissez le mode de conversion :");
+                System.out.println("1 - Convertisseur °C - °F");
+                System.out.println("2 - Convertisseur °F - °C");
+
+                mode = sc.nextLine().charAt(0);
+
+                if (mode != '1' && mode != '2')
+                    System.out.println("Mode inconnu, veuillez réitérer votre choix.");
+
+            } while (mode != '1' && mode != '2');
+
+            System.out.println("Température à convertir :");
+            temperature = sc.nextDouble();
+            sc.nextLine();
+
+            if (mode == '1') { // °C to °F
+                conversion = arrondi(((9.0 / 5.0) * temperature) + 32.0, 2);
+                System.out.println(temperature + "°C correspond à : " + conversion + "°F");
+            } else { // °F to °C
+                conversion = arrondi(((temperature - 32) * 5) / 9, 2);
+                System.out.println(temperature + "°F correspond à : " + conversion + "°C");
+            }
+
+            do {
+                System.out.println("Souhaitez-vous convertir une autre température ? (O/N)");
+                reponse = sc.nextLine().charAt(0);
+            } while (reponse != 'O' && reponse != 'N' && reponse != 'o' && reponse != 'n');
+
+        } while (reponse == 'O' || reponse == 'o');
+
+        System.out.println("Au revoir !");
+    }
+
+    public static double arrondi(double A, int B) {
+        return (double) ((int) (A * Math.pow(10, B) + .5)) / Math.pow(10, B);
     }
 }
