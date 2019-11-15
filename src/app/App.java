17006fd236f652b1ddb960f703f5435219e6feed
LR-LR <elhaire.aboudou@outlook.com>
2019-11-15 13:38:51 +0100

    suite

--- src/app/App.java
+++ src/app/App.java
@@ -2,6 +2,8 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        int a = 10, b = 20;
+        int max = (a < 20) ? ((b < 20) ? b * 2 : ((b > 20) ? b % 3 : b / 4)) : ((a == 10) ? a / 2 : a % 3);
+        System.out.println(max);
     }
 }
