7aabb7d64bd12e2c4a10d637f9439b24b503457e
LR-LR <elhaire.aboudou@gmail.com>
2019-11-17 20:47:22 +0100

    exceptions

---
+++ src/com/sdz/Main.java
@@ -0,0 +1,12 @@
+package com.sdz;
+
+/**
+ * Main
+ */
+public class Main {
+  public static void main(String[] args) {
+    int j = 20, i = 0;
+    System.out.println(j / i);
+    System.out.println("coutou toi !");
+  }
+}
