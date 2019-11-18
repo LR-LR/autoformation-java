f44a8df0a259c5608a3c8c246875ed2dec844ded
LR-LR <elhaire.aboudou@outlook.com>
2019-11-18 17:21:19 +0100

    enumerations

--- src/com/sdz/Main.java
+++ src/com/sdz/Main.java
@@ -4,13 +4,11 @@ package com.sdz;
  * Main
  */
 public class Main {
-  public static void main(String[] args) {
-    try {
-      System.out.println(" =>" + (1 / 0));
-    } catch (ClassCastException e) {
-      e.printStackTrace();
-    } finally {
-      System.out.println("action faite systématiquement");
-    }
+  public static void main(String args[]) {
+    Langage l1 = Langage.JAVA;
+    Langage l2 = Langage.PHP;
+
+    l1.getEditor();
+    l2.getEditor();
   }
 }
